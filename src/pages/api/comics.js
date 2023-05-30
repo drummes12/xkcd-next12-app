c6e6db9f409cbe515353368ebd73b5230ecc0b9f
egonzalez_kavak <esteban.gonzalez.ext@kavak.com>
2023-05-30 11:27:21 -0500

    🐛 fix: readdir comics

--- src/pages/api/comics.js
+++ src/pages/api/comics.js
@@ -1,11 +1,11 @@
 import fs from 'fs/promises'
 
-export default async function handler(req, res) {
-  const files = await fs.readdir('./comics')
+export default async function handler(_, res) {
+  const files = await fs.readdir('../../../comics')
   const lastestComicsFiles = files.slice(-8, files.length)
 
   const promisesReadFiles = lastestComicsFiles.map(async (file) => {
-    const content = await fs.readFile(`./comics/${file}`, 'utf8')
+    const content = await fs.readFile(`../../../comics/${file}`, 'utf8')
     return JSON.parse(content)
   })
   const lastestComics = await Promise.all(promisesReadFiles)
